104847b593d380cc117c3daa39a9d7cd1bfd732f
jeffersoncarvalho <jeffersoncarvalho@gmail.com>
2019-05-10 14:57:43 -0300

    aula crud

--- AULA/projeto-mini/src/app/app.module.ts
+++ AULA/projeto-mini/src/app/app.module.ts
@@ -1,9 +1,18 @@
+import { ListUserComponent } from './core/list-user/list-user.component';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RegisterUserComponent } from './core/register-user/register-user.component';
+
+const routes:Routes = [
+  {path:'',component:RegisterUserComponent},
+  {path:'list/user',component:ListUserComponent},
+  {path:'register/user',component:RegisterUserComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CoreModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
