7ae03cc4ad1f373ce6707b795d1cf31370a17541
jeffersoncarvalho <jeffersoncarvalho@gmail.com>
2019-05-29 16:03:53 -0300

    //express para user

--- AULA/projeto-mini/src/app/services/user.service.ts
+++ AULA/projeto-mini/src/app/services/user.service.ts
@@ -13,32 +13,34 @@ export class UserService {
   constructor(private httpClient:HttpClient) { }
 
   register(user:User):Observable<User>{
-    return this.httpClient.post<User>(this.url,user);  
+    //return this.httpClient.post<User>(this.url,user);  json-server
+    return this.httpClient.post<User>(`${this.url}/register`,user); //express 
+
   }
 
   list():Observable<User[]>{
-    return this.httpClient.get<User[]>(this.url);  //json-server
-    //return this.httpClient.get<User[]>(`${this.url}/list`);  
+    //return this.httpClient.get<User[]>(this.url);  //json-server
+    return this.httpClient.get<User[]>(`${this.url}/list`); //express 
   }
 
   delete(id:number){
-    return this.httpClient.delete(`${this.url}/${id}`); //json-server
-    //return this.httpClient.delete(`${this.url}/delete/${id}`);
+    //return this.httpClient.delete(`${this.url}/${id}`); //json-server
+    return this.httpClient.delete(`${this.url}/delete/${id}`); //express
   }
 
   retrieveById(id:number):Observable<User>{
-    return this.httpClient.get<User>(`${this.url}/${id}`); //json-server
-    //return this.httpClient.get<User>(`${this.url}/retrieve/${id}`);
+    //return this.httpClient.get<User>(`${this.url}/${id}`); //json-server
+    return this.httpClient.get<User>(`${this.url}/retrieve/${id}`); //express
   }
 
   update(user:User):Observable<User>{
-    return this.httpClient.put<User>(`${this.url}/${user.id}`,user); //json-server
-    //return this.httpClient.put<User>(`${this.url}/update/${user.id}`,user);
+    //return this.httpClient.put<User>(`${this.url}/${user.id}`,user); //json-server
+    return this.httpClient.put<User>(`${this.url}/update/${user.id}`,user); //express
   }
 
   retrieveByLogin(login:string):Observable<User[]>{
-    return this.httpClient.get<User[]>(`${this.url}/?login=${login}`); //json-server
-    //return this.httpClient.get<User[]>(`${this.url}/retrieve/login/${login}`);
+    //return this.httpClient.get<User[]>(`${this.url}/?login=${login}`); //json-server
+    return this.httpClient.get<User[]>(`${this.url}/retrieve/login/${login}`); //express
   }
 
 }
